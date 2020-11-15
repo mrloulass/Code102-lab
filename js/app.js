51a70a00595da038aebdb341d94d0c6c99e7f84d
mrloulass <louislassegue@gmail.com>
2020-11-14 19:03:22 -0800

    add js

---
+++ js/app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+let burger = prompt('What is your farvorite burger place?');
+
+alert('This user likes ' + burger);
+
+document.write( '<section>' + burger + '</section>');
+
+let resonse = '';
+
+
+
+
+
+
